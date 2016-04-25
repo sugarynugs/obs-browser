e871453c1aacbb88a07fb54fbce0b5176f3e0fbf
sugarynugs <info@sugarynugs.co.uk>
2016-04-25 14:38:12 +0100

    remove sugarynugs cache_path change

--- obs-browser/browser-manager-base.cpp
+++ obs-browser/browser-manager-base.cpp
@@ -337,15 +337,6 @@ std::string getBootstrap()
 #endif
 }
 
-std::string getCachePath()
-{
-#ifdef _WIN32
-	return "c:/temp";
-#else
-	return "/tmp";
-#endif
-}
-
 void BrowserManager::Impl::BrowserManagerEntry()
 {
 	std::string bootstrapPath = getBootstrap();
@@ -358,7 +349,6 @@ void BrowserManager::Impl::BrowserManagerEntry()
 		settings.no_sandbox = true;
 		CefString(&settings.cache_path).FromASCII(obs_module_config_path(""));
 		CefString(&settings.browser_subprocess_path) = getBootstrap();
-		CefString(&settings.cache_path) = getCachePath();
 		CefRefPtr<BrowserApp> app(new BrowserApp());
 		CefExecuteProcess(mainArgs, app, nullptr);
 		CefInitialize(mainArgs, settings, app, nullptr);
